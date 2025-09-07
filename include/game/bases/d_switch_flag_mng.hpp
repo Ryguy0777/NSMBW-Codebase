e6d64564a6b4da736597f7697c8137cc14965fc6
Ryguy <springtrapawesomemr.r@gmail.com>
2025-09-07 13:17:03 -0500

    Update d_switch_flag_mng.hpp

--- include/game/bases/d_switch_flag_mng.hpp
+++ include/game/bases/d_switch_flag_mng.hpp
@@ -1,12 +1,10 @@
-#include <kamek.h>
+#pragma once
+#include <types.h>
 
 class dSwitchFlagMng_c {
 public:
     u64 mFlags;
     u8 mPad[0x6C8];
-
-    // Unofficial name
-    void set(u32 eventId, int timer, bool activateEvent, bool resetEvent, bool isTimed, bool musicInterruption);
 
     static dSwitchFlagMng_c* m_instance;
 };
