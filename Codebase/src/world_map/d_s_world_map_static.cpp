5818be4b4eb57fb75ed52629f548de30ae355320
mandy <67531582+MandyIGuess@users.noreply.github.com>
2026-01-19 23:31:59 -0500

    Allow Tower/Castle 2 to have Star Coins

--- Codebase/src/world_map/d_s_world_map_static.cpp
+++ Codebase/src/world_map/d_s_world_map_static.cpp
@@ -1,4 +1,5 @@
 #include <kamek.h>
+#include <constants/game_constants.h>
 #include <game/bases/d_res_mng.hpp>
 #include <game/bases/d_s_world_map_static.hpp>
 
@@ -27,3 +28,29 @@ kmWrite16(0x80927912, ARRAY_SIZE(sc_GlobalResList)); // Delete count
 #ifdef REMOVE_LETTER_TOAD
 kmWrite32(0x80927064, 0x48000018);
 #endif
+
+// Get level from point name, used to assign Star Coin flag to courses
+// This adds proper support for Tower/Castle 2 slots
+kmBranchDefCpp(0x80928510, NULL, int, dScWMap_c *, u8 *pointName) {
+	if (*pointName != 'W') {
+		return STAGE_COUNT;
+	}
+	u8 type = pointName[2];
+
+	if (type - '0' < 10) {
+		return pointName[3] - 49;
+	}
+	switch (type) {
+		case 'T':
+			return STAGE_TOWER + (pointName[3] - '0');
+		case 'C':
+			return STAGE_CASTLE + (pointName[3] - '0');
+		case 'G':
+			return STAGE_GHOST_HOUSE;
+		case 'A':
+			return STAGE_DOOMSHIP;
+		default:
+			break;
+	}
+	return STAGE_COUNT; // Invalid
+}
