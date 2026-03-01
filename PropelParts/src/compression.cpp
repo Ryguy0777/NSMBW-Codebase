7043e550e2fa6adb807519c732dbe284b46e5471
Palapeli <26661008+mkwcat@users.noreply.github.com>
2026-02-28 21:35:24 -0500

    Fix LH and RL compression crashing

--- PropelParts/src/compression.cpp
+++ PropelParts/src/compression.cpp
@@ -1,7 +1,6 @@
 #include <cstdalign>
 #include <cstddef>
 #include <game/mLib/m_dvd.hpp>
-#include <iterator>
 #include <kamek.h>
 #include <lib/revolution/dvd.h>
 #include <propelparts/game_config.h>
@@ -12,6 +11,9 @@ extern mDvd::TUncompressInfo_c<EGG::StreamDecompLZ> s_UncompressInfoLZ;
 extern mDvd::TUncompressInfo_c<EGG::StreamDecompLH> s_UncompressInfoLH;
 extern mDvd::TUncompressInfo_c<EGG::StreamDecompLRC> s_UncompressInfoLRC;
 extern mDvd::TUncompressInfo_c<EGG::StreamDecompRL> s_UncompressInfoRL;
+
+extern mDvd::UncompressInfo_c *const *s_UncompressInfoTable_begin;
+extern mDvd::UncompressInfo_c *const *s_UncompressInfoTable_end;
 
 extern bool isAutoStreamDecomp();
 extern int ConvertPathToEntrynumASD_(const char *name, u8 *outType);
@@ -46,17 +48,22 @@ const mDvd::UncompressInfo_c *const l_AutoStreamDecompInfoArray[] = {
 } // namespace
 
 kmWritePointer(&mDvd::UncompressInfo_c::m_UnionObjectBuffer, &l_UnionObjectBufferData);
+kmWritePointer(&mDvd::s_UncompressInfoTable_begin, &l_AutoStreamDecompInfoArray[0]);
+kmWritePointer(
+  &mDvd::s_UncompressInfoTable_end,
+  &l_AutoStreamDecompInfoArray
+    [sizeof(l_AutoStreamDecompInfoArray) / sizeof(l_AutoStreamDecompInfoArray[0])]
+);
 
 kmBranchDefCpp(mDvd::initDefaultAutoStreamDecompInfo, 0, void) {
-    mDvd::initAutoStreamDecompInfo(
-      l_AutoStreamDecompInfoArray,
-      l_AutoStreamDecompInfoArray + std::size(l_AutoStreamDecompInfoArray)
-    );
+    // Prevent from overwriting our custom table with the default one IF this function is called
+    // after our patches, which is not how it works currently
+    return;
 }
 
 kmBranchDefCpp(mDvd::ConvertPathToEntrynum, 0, int, const char *path, u8 *outType) {
     // Change: Consider compressed files before uncompressed ones. This makes it easier to replace
-    // uncompressed files with compressed ones, as Riivolution doesn't support unlinking files.
+    // uncompressed files with compressed ones, as Riivolution doesn't support unlinking files
     int num = -1;
     if (mDvd::isAutoStreamDecomp()) {
         num = mDvd::ConvertPathToEntrynumASD_(path, outType);
@@ -69,3 +76,10 @@ kmBranchDefCpp(mDvd::ConvertPathToEntrynum, 0, int, const char *path, u8 *outTyp
     }
     return num;
 }
+
+// CXReadUncompLH: prevent returning -3. This appears to be some kind of "not enough data provided"
+// error that EGG doesn't properly respond to
+kmWriteNop(0x801D7884);
+
+// CXReadUncompRL: Same story as above, except we need to manually load the typical return value
+kmWrite32(0x801D6C50, 0x80630004); // lwz r3, 0x4(r3)
